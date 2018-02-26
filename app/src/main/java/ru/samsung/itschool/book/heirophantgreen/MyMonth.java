8bde9622442ebd5db99644542d079609c5887fac
EliteHacker228 <mchappycraft@yandex.ru>
2018-02-26 23:51:28 +0500

    4.3.4
    ...

---
+++ app/src/main/java/ru/samsung/itschool/book/heirophantgreen/MyMonth.java
@@ -0,0 +1,12 @@
+package ru.samsung.itschool.book.heirophantgreen;
+
+/**
+ * Created by Максим on 24.02.2018.
+ */
+
+public class MyMonth {
+    String month = ""; // Название месяца
+    double temp = 0.; // Средняя температура
+    int days = 0; // Количество дней
+    boolean like = true; // Нравится месяц
+}
